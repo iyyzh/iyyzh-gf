0b34133d9ee6139396a7c8567213172077133efb
fang <fanglehang@icloud.com>
2022-07-08 22:02:21 +0800

    sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #局部变量(执⾏⽂件名称), 根据⾃⼰项⽬随便写
 project_name="iyyzh"
 #杀掉之前正在运⾏的程序
-go_id=$(ps -ef | grep "./iyyzh" | grep -v "grep" | awk '{print $2}')
+go_id=$(ps -ef | grep "./${project_name}" | grep -v "grep" | awk '{print $2}')
 if [ -z "$go_id" ]; then
   echo "[go pid not found]"
 else
@@ -11,18 +11,16 @@ else
 fi
 #清除旧的编译⽂件
 echo "clean old file"
-rm -rf iyyzh
+rm -rf ${project_name}
 #执⾏⽇志，根据⾃⼰项⽬情况可选
-rm -rf iyyzh.log
+rm -rf ${project_name}.log
 if [ -f main ]; then
   echo "strat new process"
-  mv main iyyzh
-  chmod -R 777 iyyzh
+  mv main ${project_name}
+  chmod -R 777 ${project_name}
   #这⾥要防⽌nohup不执⾏，添加了⼀个BUILD_ID
-#  BUILD_ID=DONTKILLME nohup ./${project_name} >${project_name}.log 2>&1 &
-#  nohup ./${project_name} >${project_name}.log 2>&1 &
-  ./iyyzh
-  echo "test ./${project_name} over"
+  BUILD_ID=DONTKILLME nohup ./${project_name} >${project_name}.log 2>&1 &
+  echo "test BUILD_ID=DONTKILLME nohup ./${project_name} >${project_name}.log 2>&1 & over"
 else
   echo "executable file not found,quit"
 fi
